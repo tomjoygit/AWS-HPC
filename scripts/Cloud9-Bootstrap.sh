765521f346f1825cbd0241ec257127f58ab3dc77
tomjoygit <tomjoy.pala@gmail.com>
2023-02-03 21:19:41 +0100

    Add files via upload

--- scripts/Cloud9-Bootstrap.sh
+++ scripts/Cloud9-Bootstrap.sh
@@ -71,9 +71,9 @@ fi
 sudo chmod 400 /home/ec2-user/.ssh/id_rsa
 
 #Create the cluster and wait
-/home/ec2-user/.local/bin/pcluster create-cluster --cluster-name "${CLUSTER_NAME}" --cluster-configuration config.${AWS_REGION_NAME}.yaml --rollback-on-failure false --wait
+/home/ec2-user/.local/bin/pcluster create-cluster --cluster-name "tme-${CLUSTER_NAME}" --cluster-configuration config.${AWS_REGION_NAME}.yaml --rollback-on-failure false --wait
 
-HEADNODE_PRIVATE_IP=$(/home/ec2-user/.local/bin/pcluster describe-cluster --cluster-name "${CLUSTER_NAME}" | jq -r '.headNode.privateIpAddress')
+HEADNODE_PRIVATE_IP=$(/home/ec2-user/.local/bin/pcluster describe-cluster --cluster-name "tme-${CLUSTER_NAME}" | jq -r '.headNode.privateIpAddress')
 echo "export HEADNODE_PRIVATE_IP='${HEADNODE_PRIVATE_IP}'" >> cluster_env
 
 # Modify the Message Of The Day
@@ -86,14 +86,14 @@ echo 'run-parts /etc/update-motd.d' >> /home/ec2-user/.bash_profile
 #attach the ParallelCluster SG to the Cloud9 instance (for FSx or NFS)
 INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 SG_CLOUD9=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query Reservations[*].Instances[*].SecurityGroups[*].GroupId --output text)
-SG_HEADNODE=$(aws cloudformation describe-stack-resources --stack-name "${CLUSTER_NAME}" --logical-resource-id ComputeSecurityGroup --query "StackResources[*].PhysicalResourceId" --output text)
+SG_HEADNODE=$(aws cloudformation describe-stack-resources --stack-name "tme-${CLUSTER_NAME}" --logical-resource-id ComputeSecurityGroup --query "StackResources[*].PhysicalResourceId" --output text)
 aws ec2 modify-instance-attribute --instance-id $INSTANCE_ID --groups $SG_CLOUD9 $SG_HEADNODE
 
 #increase the maximum number of files that can be handled by file watcher,
 sudo bash -c 'echo "fs.inotify.max_user_watches=524288" >> /etc/sysctl.conf' && sudo sysctl -p
 
 if [[ $FSX_ID == "AUTO" ]];then
-  FSX_ID=$(aws cloudformation describe-stack-resources --stack-name "${CLUSTER_NAME}" --logical-resource-id FSX0 --query "StackResources[*].PhysicalResourceId" --output text)
+  FSX_ID=$(aws cloudformation describe-stack-resources --stack-name "tme-${CLUSTER_NAME}" --logical-resource-id FSX0 --query "StackResources[*].PhysicalResourceId" --output text)
 fi
 
 FSX_DNS_NAME=$(aws fsx describe-file-systems --file-system-ids $FSX_ID --query "FileSystems[*].DNSName" --output text)
